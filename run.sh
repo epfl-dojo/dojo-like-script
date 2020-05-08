0a3c6aefed079c5d283432dbc53738bb84c2e6fa
Jérôme Cosandey <jerome.cosandey@gmail.com>
2020-05-08 11:58:04 +0200

    Added foreach

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-repositories=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GHTOKEN}" -s https://api.github.com/orgs/epfl-dojo/repos | jq )
-
+repositories=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GHTOKEN}" -s https://api.github.com/orgs/epfl-dojo/repos | jq '.[].name')
+echo $repositories
 repo_name=animated-broccoli
 
+for repo_name in $repositories
+do
+  echo $repo_name
+done
 
 request=$(curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GHTOKEN}" -s https://api.github.com/user/starred/epfl-dojo/${repo_name});
 
