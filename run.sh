9d2ec9a396abdc65dc45dcac4b6fdbf60f7c9103
Rocco <roccoronzano@hotmail.com>
2020-05-08 11:46:02 +0200

    Curl PUT & DELETE

--- run.sh
+++ run.sh
@@ -9,4 +9,6 @@ echo $request;
 #PUT /user/starred/:owner/:repo
 
 
-# curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GHTOKEN}" -s https://api.github.com/user/starred/epfl-dojo/animated-brocoli
+# curl -X PUT -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GHTOKEN}" -s https://api.github.com/user/starred/epfl-dojo/animated-broccoli
+
+# curl -X DELETE -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GHTOKEN}" -s https://api.github.com/user/starred/epfl-dojo/animated-broccoli
